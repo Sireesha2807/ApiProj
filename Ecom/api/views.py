2f84b39f723c72fab1e7d24be4222b78fb6a0571
Sireesha2807 <sireeshamadu2807@gmail.com>
2022-07-14 16:11:54 +0530

    get request

--- Ecom/api/views.py
+++ Ecom/api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.response import Response # for json response
 from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import Itemserializer
 
 
 @api_view(['GET'])
 def getdata(request):
-    person = {'name':'siri','age':'25'}
-    return Response(person)
+    items = Item.objects.all()
+    serializer = Itemserializer(items, many=True)
+    return Response(serializer.data)
+
+
--- Ecom/base/models.py
+++ Ecom/base/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    createdat = models.DateTimeField(auto_now_add=True)
